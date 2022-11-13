76d114517450b8c92ddc83c0f4e59786739a4fb6
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-11-13 14:53:52 +0700

    Routes Init

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 
+//Routes Import
+const testRoutes = require('./Routes/test');
+
 //Midleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 app.use(morgan('tiny'));
+
+//Routes
+app.use('/test', testRoutes);
 
 //Listen
 app.listen(8000, () => {
